55046979d0b6b64546946d84c1b1d2a0898bc48e
Sukhchain Singh <sukhsingh15666@gmail.com>
2025-05-06 23:31:06 +0530

    Update settings.py

--- schoolProject/settings.py
+++ schoolProject/settings.py
@@ -133,8 +133,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 
